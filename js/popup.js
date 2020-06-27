e27515d0970ee633cf001a4ace822e603d025dcf
deepazone <deepazone@yandex.ru>
2020-06-27 17:06:54 +0300

    Исправил ошибки 1

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,52 @@
-var popUpOpen = document.querySelector(".search");
+var searchButton = document.querySelector(".search");
 var formSearch = document.querySelector(".form-search");
-var formSearchOpen = document.querySelector(".form-search-open");
-var SearchSubmit = document.querySelector(".search-submit");
-var dateIn = document.querySelector("[name=date-in]")
-var dateOut = document.querySelector("[name=date-out]")
+var dateIn = document.querySelector("[name=date-in]");
+var dateOut = document.querySelector("[name=date-out]");
 
-popUpOpen.addEventListener("click", function(evt) {
+var isStorageSupport = true;
+var storage = "";
+
+try {
+	storage = localStorage.getItem("date-in");
+ } catch (err) {
+	isStorageSupport = false;
+ }
+
+searchButton.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	formSearch.classList.toggle("form-search-open");
 });
 
+if (storage) {
+	dateIn.value = storage;
+ }
+
 formSearch.addEventListener("submit", function(evt) {
-	if (!dateIn.value || !dateOut.value) {
+	if (!dateIn.value) {
 		evt.preventDefault();
 		console.log('Введите значение');
-	}
+		formSearch.classList.add('shake-error');
+		formSearch.offsetWidth = formSearch.offsetWidth;
+		dateIn.focus();
+	} else {
+		if (isStorageSupport) {
+		  localStorage.setItem("date-in", dateIn.value);
+		  console.log('Форма отправлена');
+		}
+	}	
+});
+
+formSearch.addEventListener("submit", function(evt) {
+	if (!dateOut.value) {
+		evt.preventDefault();
+		console.log('Введите значение');
+		formSearch.classList.add('shake-error');
+		formSearch.offsetWidth = formSearch.offsetWidth;
+		dateOut.focus();
+	} else {
+		if (isStorageSupport) {
+		  localStorage.setItem("date-out", dateOut.value);
+		  console.log('Форма отправлена');
+		}
+	}	
 });
